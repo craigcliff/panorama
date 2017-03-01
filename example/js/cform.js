8139c8b2da646389848a312c7de4cf94c2b447e5
craigcliff <craigcliff@gmail.com>
2017-03-01 16:10:28 +0200

    we4r
    trest test

---
+++ example/js/cform.js
@@ -0,0 +1,13 @@
+var frm = $('#cform');
+    frm.submit(function (ev) {
+        $.ajax({
+            type: frm.attr('method'),
+            url: frm.attr('action'),
+            data: frm.serialize(),
+            success: function (data) {
+                $('#cform').append("<div class='alert alert-success'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>×</button><i class='fa fa-thumbs-o-up'></i><strong>Well done!</strong> Your Message Submitted!</div>")
+            }
+        });
+
+        ev.preventDefault();
+});		
